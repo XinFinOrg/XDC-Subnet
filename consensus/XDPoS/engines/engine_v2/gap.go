fae0d0155f895dedd1e3edad68c07e06898e3836
Wang Gerui <wgr523@gmail.com>
2023-04-09 19:09:55 +0800

    fix gapPlusOne bug: block number 1 is also gapPlusOne

--- consensus/XDPoS/engines/engine_v2/gap.go
+++ consensus/XDPoS/engines/engine_v2/gap.go
@@ -4,5 +4,9 @@ import "github.com/XinFinOrg/XDPoSChain/core/types"
 
 // the block after gap block, whose NextEpoch validators should not be empty
 func (x *XDPoS_v2) IsGapPlusOneBlock(header *types.Header) bool {
+	if header.Number.Uint64() == 1 {
+		// prevent overflow of number - number%x.config.Epoch - x.config.Gap < 0
+		return true
+	}
 	return (header.Number.Uint64() % x.config.Epoch) == (x.config.Epoch - x.config.Gap + 1)
 }
