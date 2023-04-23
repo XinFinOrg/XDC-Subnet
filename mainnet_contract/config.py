17541c86bfd739769336b5cffdb524a5ab463914
span14 <span14@illinois.edu>
2023-04-22 20:00:12 -0400

    update meta information

--- mainnet_contract/config.py
+++ mainnet_contract/config.py
@@ -1,4 +1,4 @@
-NODE_RPC = "http://144.126.140.1:8545"
+NODE_RPC = "http://194.233.77.19:8545"
 DEPLOY_INIT_JSON = "./subnet_initialization.json"
 SUBNET_CONTRACT_JSON = "./build/contracts/Subnet.json"
 HEADER_CONTRACT_JSON = "./build/contracts/HeaderReader.json"
