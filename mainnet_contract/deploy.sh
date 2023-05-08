c13b339ed20ba17201715afe45e36677628ce60f
Liam Lai <liam.icheng.lai@gmail.com>
2023-05-08 14:52:20 +1000

    automation deployment

--- mainnet_contract/deploy.sh
+++ mainnet_contract/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-echo "1. initialize config"
-python contract_config_setup.py
-
-echo "2. create python virtual env"
+echo "0. create python virtual env"
 # Create and Activate python virtual env
 python -m venv xdc
 source xdc/bin/activate
 
+echo "1. initialize config"
+pip install -r requirements.txt
+python contract_config_setup.py
+
+echo "2. replace web3 to customize xdc web3"
 # Git clone the modified web3
 git clone https://github.com/span14/web3.py.git
 cd web3.py
