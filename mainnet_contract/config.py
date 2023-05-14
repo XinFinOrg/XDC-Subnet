1eb06f4349a77d108412888f5d774d0ca85bcb89
Liam <liam.icheng.lai@gmail.com>
2023-05-14 12:41:07 +1000

    Use Docker to deploy contract (#25)
    * automation deployment
    
    * automation deployment
    
    * automation deployment
    
    * chmod 755 on deploy.sh
    
    * auto deployment
    
    * automation deployment
    update genesis file

--- mainnet_contract/config.py
+++
@@ -1,4 +0,0 @@
-NODE_RPC = "http://194.233.77.19:8545"
-DEPLOY_INIT_JSON = "./subnet_initialization.json"
-SUBNET_CONTRACT_JSON = "./build/contracts/Subnet.json"
-HEADER_CONTRACT_JSON = "./build/contracts/HeaderReader.json"
