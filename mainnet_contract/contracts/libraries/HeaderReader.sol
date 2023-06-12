68d7734f452371cd5aa64ec203df707ff11104e9
tradergalax <429224280@qq.com>
2023-06-12 22:38:25 +0800

    update solidiry

--- mainnet_contract/contracts/libraries/HeaderReader.sol
+++ mainnet_contract/contracts/libraries/HeaderReader.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.20 <0.9.0;
-pragma experimental ABIEncoderV2;
 
 // HeaderReader is a helper library to read fields out of rlp-encoded blocks.
 // It is mainly consisted of Solidity-RLP(https://github.com/hamdiallam/Solidity-RLP) and
