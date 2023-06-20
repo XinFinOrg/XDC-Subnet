6066c8540b979c8c8576cf8517b065e5877683ba
tradergalax <429224280@qq.com>
2023-06-20 21:11:30 +0800

    Update subnet.js

--- mainnet_contract/test/subnet.js
+++ mainnet_contract/test/subnet.js
@@ -112,7 +112,8 @@ const composeAndSignBlock = (
   validators,
   threshold,
   current,
-  next
+  next,
+  penalties = []
 ) => {
   const version = new Uint8Array([2]);
   const voteForSignHash = ethers.utils.keccak256(
@@ -148,7 +149,7 @@ const composeAndSignBlock = (
     validator: new Uint8Array(8),
     validators: current,
     nextValidators: next,
-    penalties: [],
+    penalties: penalties,
   };
 
   var blockBuffer = Buffer.from(
@@ -174,7 +175,7 @@ const composeAndSignBlock = (
       new Uint8Array(8),
       current,
       next,
-      [],
+      penalties,
     ])
   );
 
@@ -568,6 +569,133 @@ describe("Subnet", () => {
       const block2HeaderResp = await custom.getHeaderByNumber(2);
       expect(block2HeaderResp[0]).to.eq(block2Hash);
       expect(block2HeaderResp[1]).to.eq(2);
+    });
+
+    it("penalty validity verify", async () => {
+      const [block2, block2Encoded, block2Hash] = composeAndSignBlock(
+        2,
+        2,
+        1,
+        customeBlock1["hash"],
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const [block3, block3Encoded, block3Hash] = composeAndSignBlock(
+        3,
+        3,
+        2,
+        block2Hash,
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const [block4, block4Encoded, block4Hash] = composeAndSignBlock(
+        4,
+        4,
+        3,
+        block3Hash,
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const [block5, block5Encoded, block5Hash] = composeAndSignBlock(
+        5,
+        5,
+        4,
+        block4Hash,
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const newValidators = createValidators(5);
+      const penalties = [newValidators[0], newValidators[1]];
+      const actualValidators = [
+        newValidators[2],
+        newValidators[3],
+        newValidators[4],
+      ];
+
+      const [block6, block6Encoded, block6Hash] = composeAndSignBlock(
+        6,
+        6,
+        5,
+        block5Hash,
+        customValidators,
+        2,
+        [],
+        newValidators.map((item) => item.address),
+        penalties.map((item) => item.address)
+      );
+      const [block7, block7Encoded, block7Hash] = composeAndSignBlock(
+        7,
+        7,
+        6,
+        block6Hash,
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const [block8, block8Encoded, block8Hash] = composeAndSignBlock(
+        8,
+        8,
+        7,
+        block7Hash,
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const [block9, block9Encoded, block9Hash] = composeAndSignBlock(
+        9,
+        9,
+        8,
+        block8Hash,
+        customValidators,
+        2,
+        [],
+        []
+      );
+      const [block10, block10Encoded, block10Hash] = composeAndSignBlock(
+        10,
+        10,
+        9,
+        block9Hash,
+        actualValidators,
+        2,
+        actualValidators.map((item) => item.address),
+        []
+      );
+      await custom.receiveHeader([block2Encoded, block3Encoded, block4Encoded]);
+
+      await custom.receiveHeader([block5Encoded, block6Encoded, block7Encoded]);
+
+      await custom.receiveHeader([
+        block8Encoded,
+        block9Encoded,
+        block10Encoded,
+      ]);
+      const block7Resp = await custom.getHeader(block7Hash);
+      expect(block7Resp[0]).to.eq(block6Hash);
+      expect(block7Resp[1]).to.eq(7);
+      expect(block7Resp[2]).to.eq(7);
+      expect(block7Resp[4]).to.eq(true);
+      const latestBlocks = await custom.getLatestBlocks();
+      expect(latestBlocks[0][0]).to.eq(block10Hash);
+      expect(latestBlocks[1][0]).to.eq(block7Hash);
+
+      const blockHeader7Resp = await custom.getHeaderByNumber(7);
+      expect(blockHeader7Resp[0]).to.eq(block7Hash);
+      expect(blockHeader7Resp[1]).to.eq(7);
+
+      const blockHeader8Resp = await custom.getHeaderByNumber(8);
+      expect(blockHeader8Resp[0]).to.eq(block8Hash);
+      expect(blockHeader8Resp[1]).to.eq(8);
     });
   });
 });
