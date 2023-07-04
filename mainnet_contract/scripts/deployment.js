91895310b2133f19a25b7f9bd6b1f096436b74f5
tradergalax <429224280@qq.com>
2023-07-04 09:32:43 +0800

    deployment HexRLP

--- mainnet_contract/scripts/deployment.js
+++ mainnet_contract/scripts/deployment.js
@@ -27,11 +27,8 @@ async function main() {
   });
   const data0 = await block0res.json();
   const data1 = await block1res.json();
-
-  const data0Encoded =
-    "0x" + Buffer.from(data0["result"]["EncodedRLP"], "base64").toString("hex");
-  const data1Encoded =
-    "0x" + Buffer.from(data1["result"]["EncodedRLP"], "base64").toString("hex");
+  const data0Encoded = "0x" + data0["result"]["HexRLP"];
+  const data1Encoded = "0x" + data1["result"]["HexRLP"];
   // console.log({ data0Encoded, data1Encoded });
 
   const headerReaderFactory = await hre.ethers.getContractFactory(
