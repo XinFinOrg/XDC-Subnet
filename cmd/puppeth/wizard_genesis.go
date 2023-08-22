03e6a97d5d52c6b26edd6682b4b9efa5793e3ea9
Wanwiset Peerapatanapokin <wanwiset25@gmail.com>
2023-08-22 14:27:27 +0400

    match gerui branch

--- cmd/puppeth/wizard_genesis.go
+++ cmd/puppeth/wizard_genesis.go
@@ -22,9 +22,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math/rand"
-	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/XinFinOrg/XDC-Subnet/common"
@@ -44,21 +41,6 @@ import (
 
 // makeGenesis creates a new genesis struct based on some user input.
 func (w *wizard) makeGenesis() {
-	// Make sure we have a good network name to work with	fmt.Println()
-	// Docker accepts hyphens in image names, but doesn't like it for container names
-	if w.network == "" {
-		fmt.Println("Please specify a network name to administer (no spaces or hyphens, please)")
-		for {
-			w.network = w.readString()
-			if !strings.Contains(w.network, " ") && !strings.Contains(w.network, "-") {
-				fmt.Printf("\nSweet, you can set this via --network=%s next time!\n\n", w.network)
-				break
-			}
-			log.Error("I also like to live dangerously, still no spaces or hyphens")
-		}
-	}
-	log.Info("Administering Ethereum network", "name", w.network)
-
 	// Set logger level to avoid log spam
 	log.Root().SetHandler(log.LvlFilterHandler(log.LvlWarn, log.Root().GetHandler()))
 	// Construct a default genesis block
@@ -320,7 +302,6 @@ func (w *wizard) manageGenesis() {
 		log.Info("Genesis block destroyed")
 
 		w.conf.Genesis = nil
-		w.conf.path = filepath.Join(os.Getenv("HOME"), ".puppeth", w.network)
 		w.conf.flush()
 
 	default:
