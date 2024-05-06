0e8a8f65382e5e353688a2154a12d25cbdc23448
wanwiset25 <wanwiset25@gmail.com>
2024-05-06 13:27:34 +0400

    fix test

--- consensus/tests/engine_v2_tests/api_test.go
+++ consensus/tests/engine_v2_tests/api_test.go
@@ -11,18 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGetMissedRoundsInEpochByBlockNumOnlyForV2Consensus(t *testing.T) {
-	_, bc, _, _, _ := PrepareXDCTestBlockChainWith128Candidates(t, 1802, params.TestXDPoSMockChainConfig)
-
-	engine := bc.GetBlockChain().Engine().(*XDPoS.XDPoS)
-	blockNum := rpc.BlockNumber(123)
-
-	data, err := engine.APIs(bc.GetBlockChain())[0].Service.(*XDPoS.API).GetMissedRoundsInEpochByBlockNum(&blockNum)
-
-	assert.EqualError(t, err, "Not supported in the v1 consensus")
-	assert.Nil(t, data)
-}
-
 func TestGetMissedRoundsInEpochByBlockNumReturnEmptyForV2(t *testing.T) {
 	_, bc, cb, _, _ := PrepareXDCTestBlockChainWith128Candidates(t, 1802, params.TestXDPoSMockChainConfig)
 
@@ -32,7 +20,7 @@ func TestGetMissedRoundsInEpochByBlockNumReturnEmptyForV2(t *testing.T) {
 	data, err := engine.APIs(bc.GetBlockChain())[0].Service.(*XDPoS.API).GetMissedRoundsInEpochByBlockNum(&blockNum)
 
 	assert.Nil(t, err)
-	assert.Equal(t, types.Round(900), data.EpochRound)
+	assert.Equal(t, types.Round(1800), data.EpochRound)
 	assert.Equal(t, big.NewInt(1800), data.EpochBlockNumber)
 	assert.Equal(t, 0, len(data.MissedRounds))
 
@@ -41,7 +29,7 @@ func TestGetMissedRoundsInEpochByBlockNumReturnEmptyForV2(t *testing.T) {
 	data, err = engine.APIs(bc.GetBlockChain())[0].Service.(*XDPoS.API).GetMissedRoundsInEpochByBlockNum(&blockNum)
 
 	assert.Nil(t, err)
-	assert.Equal(t, types.Round(900), data.EpochRound)
+	assert.Equal(t, types.Round(1800), data.EpochRound)
 	assert.Equal(t, big.NewInt(1800), data.EpochBlockNumber)
 	assert.Equal(t, 0, len(data.MissedRounds))
 
@@ -50,22 +38,22 @@ func TestGetMissedRoundsInEpochByBlockNumReturnEmptyForV2(t *testing.T) {
 	data, err = engine.APIs(bc.GetBlockChain())[0].Service.(*XDPoS.API).GetMissedRoundsInEpochByBlockNum(&blockNum)
 
 	assert.Nil(t, err)
-	assert.Equal(t, types.Round(900), data.EpochRound)
+	assert.Equal(t, types.Round(1800), data.EpochRound)
 	assert.Equal(t, big.NewInt(1800), data.EpochBlockNumber)
 	assert.Equal(t, 0, len(data.MissedRounds))
 }
 
 func TestGetMissedRoundsInEpochByBlockNumReturnEmptyForV2FistEpoch(t *testing.T) {
-	_, bc, _, _, _ := PrepareXDCTestBlockChainWith128Candidates(t, 1802, params.TestXDPoSMockChainConfig)
+	_, bc, _, _, _ := PrepareXDCTestBlockChainWith128Candidates(t, 10, params.TestXDPoSMockChainConfig)
 
 	engine := bc.GetBlockChain().Engine().(*XDPoS.XDPoS)
-	blockNum := rpc.BlockNumber(901)
+	blockNum := rpc.BlockNumber(2)
 
 	data, err := engine.APIs(bc.GetBlockChain())[0].Service.(*XDPoS.API).GetMissedRoundsInEpochByBlockNum(&blockNum)
 
 	assert.Nil(t, err)
-	assert.Equal(t, types.Round(1), data.EpochRound)
-	assert.Equal(t, big.NewInt(901), data.EpochBlockNumber)
+	assert.Equal(t, types.Round(0), data.EpochRound)
+	assert.Equal(t, big.NewInt(0), data.EpochBlockNumber)
 	assert.Equal(t, 0, len(data.MissedRounds))
 }
 
@@ -77,8 +65,8 @@ func TestGetMissedRoundsInEpochByBlockNum(t *testing.T) {
 
 	startingBlockNum := currentBlock.Number().Int64() + 1
 	// Skipped the round
-	roundNumber := startingBlockNum - chainConfig.XDPoS.V2.SwitchBlock.Int64() + 2
-	block := CreateBlock(blockchain, chainConfig, currentBlock, int(startingBlockNum), roundNumber, blockCoinBase, signer, signFn, nil, nil, "b345a8560bd51926803dd17677c9f0751193914a851a4ec13063d6bf50220b53")
+	roundNumber := startingBlockNum + 2
+	block := CreateBlock(blockchain, chainConfig, currentBlock, int(startingBlockNum), roundNumber, blockCoinBase, signer, signFn, nil, nil, "c2bf7b59be5184fc1148be5db14692b2dc89a1b345895d3e8d0ee7b8a7607450")
 	err := blockchain.InsertBlock(block)
 	if err != nil {
 		t.Fatal(err)
@@ -95,15 +83,15 @@ func TestGetMissedRoundsInEpochByBlockNum(t *testing.T) {
 	data, err := engine.APIs(bc.GetBlockChain())[0].Service.(*XDPoS.API).GetMissedRoundsInEpochByBlockNum(&blockNum)
 
 	assert.Nil(t, err)
-	assert.Equal(t, types.Round(900), data.EpochRound)
+	assert.Equal(t, types.Round(1800), data.EpochRound)
 	assert.Equal(t, big.NewInt(1800), data.EpochBlockNumber)
 	assert.Equal(t, 2, len(data.MissedRounds))
 	assert.NotEmpty(t, data.MissedRounds[0].Miner)
-	assert.Equal(t, data.MissedRounds[0].Round, types.Round(903))
+	assert.Equal(t, data.MissedRounds[0].Round, types.Round(1803))
 	assert.Equal(t, data.MissedRounds[0].CurrentBlockNum, big.NewInt(1803))
 	assert.Equal(t, data.MissedRounds[0].ParentBlockNum, big.NewInt(1802))
 	assert.NotEmpty(t, data.MissedRounds[1].Miner)
-	assert.Equal(t, data.MissedRounds[1].Round, types.Round(904))
+	assert.Equal(t, data.MissedRounds[1].Round, types.Round(1804))
 	assert.Equal(t, data.MissedRounds[0].CurrentBlockNum, big.NewInt(1803))
 	assert.Equal(t, data.MissedRounds[0].ParentBlockNum, big.NewInt(1802))
 
